7e0489a75505e6ec89ed447cb234496597dfd537
T-twice <thandomsibi7@gmail.com>
2018-09-19 11:32:15 +0200

    thando changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { ProfilePage } from '../pages/profile/profile';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = CategoryPage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
