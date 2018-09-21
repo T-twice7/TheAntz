94ad801845f32a97d7add7cd9599d61406c0b7b9
T-twice <thandomsibi7@gmail.com>
2018-09-21 08:16:11 +0200

    twice changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { ProfilePage } from '../pages/profile/profile';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = EditProfilePage;
+  rootPage: any = CategoryPage;
 
   pages: Array<{title: string, component: any}>;
 
