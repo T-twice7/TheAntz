bc304ba0975d72689b41bc7333bddeef9b71b58a
T-twice <thandomsibi7@gmail.com>
2018-10-08 14:06:54 +0200

    i did comite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ import { UploadImagePage } from '../pages/upload-image/upload-image';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = UploadImagePage;
+  rootPage: any = LoginPage;
 
   
 
