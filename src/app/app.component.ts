4dd3a78ed24993beb478b06442ff273e307249bc
T-twice <thandomsibi7@gmail.com>
2018-10-08 14:34:53 +0200

    twice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ import { UploadImagePage } from '../pages/upload-image/upload-image';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = UploadImagePage;
 
   
 
