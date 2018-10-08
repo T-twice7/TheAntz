103006567ebd95d8fe901df0a0e523d1704c2842
Leonard <spazgrafficz@outlook.com>
2018-10-08 13:50:13 +0200

    bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import { viewParentEl } from '@angular/core/src/view/util';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = CategoryPage;
+  rootPage: any = SplashPage;
 
   
 
